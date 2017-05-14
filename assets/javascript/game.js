ca978e4736358c48af32bf6d4a24f5308094c616
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-05-14 12:18:44 -0500

    javascript pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+/*At time of landing on page:
+(1) the blank spaces for a current word are displayed
+(2) number of remaining guesses is 12 
+(3) letters already guessed is blank
+*/
+
+/*Pressing any key starts the game */
+
+/* If user guesses a correct letter: 
+	(1) letter replaces appropriate blanks in word
+	(2) number of guesses remaining does not decrease
+	(3) play sound effect 
+*/
+
+/* If user guesses an incorrect letter
+	(1) letter displays under letters already guessed 
+	(2) reduces the number of guesses by 1 
+	(3) play sound effect
+*/
+
+/* If user guesses a duplicate letter, nothing happens */
+
+/* If number of guesses remaining is 0, "you're a loser" */
+
+/* If no blanks are remaining in word:
+	(1) provide a winning message
+	(2) reset game with a new word
+*/
