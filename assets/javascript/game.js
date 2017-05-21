307ff9a120484e9b5d44bba1e89310c78cbf74f1
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-05-20 23:30:50 -0500

    partially working code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -35,8 +35,51 @@ $(document).ready(function() {
   document.onkeyup = function(event) {
     userGuess = event.key;
     console.log(userGuess);
+    checkLetter();
   }
-  
+
+  /*I believe the code works up to here. In subsequent code, I was trying to test whether
+  the user's guess matched a letter in the randomly selected word. If so, I wanted to change
+  the CSS class of the corresponding letter div so that the letter would be displayed.*/
+
+  /*check if user guess is equal to letter(s) in letterArray;
+    if yes, run function to display letter
+    if no, run function to record guessed letter*/  
+  function checkLetter(userGuess){
+    var matchedLetters = [];
+    for (i = 0; i < letterArray.length; i++) {
+      if (userGuess === letterArray[i]) {
+        matchedLetters.push(i);
+        console.log(matchedLetters); // this line is not returning intended values
+        displayLetter();
+      }
+      if (matchedLetters.length = 0) {
+        recordGuessedLetter();
+      }
+    }
+  };
+
+  /*create an array containing all the letter divs that have the underscore class*/
+  var letterDivArray = $('.js-underscore');
+
+  /*create a variable to grab a single letter div*/
+  var singleLetterDiv;
+
+  /*create a function that changes the CSS class of the letter div containing the matched letter*/
+  function displayLetter() {
+    singleLetterDiv = letterDivArray(letterArray[i]);
+    singleLetterDiv.innerText = letterArray[i];
+    singleLetterDiv.removeClass('js-underscore').addClass('visible-letter');
+  };
+
+  /*create a function that records the guessed letter*/
+  function recordGuessedLetter() {
+    guessedLetterArray.push(userGuess);
+    $('.guesses').html('Letters guessed:&nbsp&nbsp' + userGuess);
+    attempts--;
+    $('.attempts').html('Guesses remaining:&nbsp&nbsp' + attempts);
+  };
+ 
 });
 
 
